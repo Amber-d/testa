43e1df25ba23e64ba3288a513df977a25ce2fc9b
Amber-d <3161347202@qq.com>
2022-06-20 11:32:40 +0800

    添加了子目录功能

--- app/src/main/java/com/example/testa/MainActivity.java
+++ app/src/main/java/com/example/testa/MainActivity.java
@@ -9,10 +9,12 @@ import androidx.core.os.EnvironmentCompat;
 
 import android.Manifest;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
@@ -29,17 +31,19 @@ import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private static final int CAMERA_REQUEST_CODE = 0x00000010;
     private static final int PERMISSION_CAMERA_REQUEST_CODE = 0x00000012;
     //自定义变量
     private ImageView imageShow;           //显示图片
-    private Bitmap bmp;                    //原始图片
+    private Bitmap bmp;
     private final int IMAGE_OPEN = 0;      //打开图片
     public ImageView ivPhoto;
     public ImageView ivCamera;
@@ -83,6 +87,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
     private void checkPermissionAndCamera() {
         int hasCameraPermission = ContextCompat.checkSelfPermission(getApplication(),
                 Manifest.permission.CAMERA);
@@ -91,7 +96,7 @@ public class MainActivity extends AppCompatActivity {
             openCamera();
         } else {
             //没有权限，申请权限。
-            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA},
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
                     PERMISSION_CAMERA_REQUEST_CODE);
         }
     }
@@ -151,30 +156,35 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) { //创建Menu
         //自定义menu 添加图标(使用自带图标)
-        menu.add(1, 1 , 1, "本地").
+        menu.add(1, 1, 1, "本地").
                 setIcon(android.R.drawable.ic_menu_slideshow);
-        menu.add(1, 2  , 2, "相机").
+        menu.add(1, 2, 2, "相机").
                 setIcon(android.R.drawable.ic_menu_view);
-        SubMenu file=menu.addSubMenu(1,3,3,"图片处理");
-        file.add(2, 1 , 3, "怀旧").
+        SubMenu file = menu.addSubMenu(1, 3, 3, "图片处理");
+        file.add(2, 1, 3, "怀旧").
                 setIcon(android.R.drawable.ic_menu_edit);
-        file.add(2, 2 , 4, "浮雕").
+        file.add(2, 2, 4, "浮雕").
                 setIcon(android.R.drawable.ic_menu_gallery);
-        file.add(2, 3 , 5, "模糊").
+        file.add(2, 3, 5, "模糊").
                 setIcon(android.R.drawable.ic_menu_crop);
-        file.add(2, 4 , 6, "光照").
+        file.add(2, 4, 6, "光照").
                 setIcon(android.R.drawable.ic_menu_camera);
-        file.add(2, 5 , 7, "锐化").
+        file.add(2, 5, 7, "锐化").
+                setIcon(android.R.drawable.ic_menu_view);
+        file.add(2, 6, 8, "油画").
+                setIcon(android.R.drawable.ic_menu_slideshow);
+        menu.add(1, 10, 9, "保存").
                 setIcon(android.R.drawable.ic_menu_view);
 
 
         return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) { //选择Menu
         //选择id 对应Menu.add的参数Menu.FIRST+i
         int id = item.getItemId();
-        if(item.getGroupId()==1) {
+        if (item.getGroupId() == 1) {
             switch (id) {
                 case 1:
                     Toast.makeText(this, "打开本地图片", Toast.LENGTH_SHORT).show();
@@ -184,9 +194,12 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(this, "打开相机", Toast.LENGTH_SHORT).show();
                     openCamera();
                     break;
+                case 10:
+                    Toast.makeText(this, "保存图片", Toast.LENGTH_SHORT).show();
+
+                    break;
             }
-        }
-        else {
+        } else {
             switch (id) {
                 case 1:
                     Toast.makeText(this, "图片怀旧效果", Toast.LENGTH_SHORT).show();
@@ -208,6 +221,10 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(this, "图片锐化效果", Toast.LENGTH_SHORT).show();
                     SharpenImage();
                     break;
+                case 6:
+                    Toast.makeText(this, "图片油画效果", Toast.LENGTH_SHORT).show();
+                    YouHua();
+                    break;
             }
         }
 
@@ -226,7 +243,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void SharpenImage() {
-        int[] laplacian = new int[] {  -1, -1, -1, -1, 9, -1, -1, -1, -1 };
+        int[] laplacian = new int[]{-1, -1, -1, -1, 9, -1, -1, -1, -1};
         int width = bmp.getWidth();
         int height = bmp.getHeight();
         Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
@@ -242,10 +259,8 @@ public class MainActivity extends AppCompatActivity {
         int[] pixels = new int[width * height];
         bmp.getPixels(pixels, 0, width, 0, 0, width, height);
         //图像处理
-        for (int i = 1; i < height - 1; i++)
-        {
-            for (int k = 1; k < width - 1; k++)
-            {
+        for (int i = 1; i < height - 1; i++) {
+            for (int k = 1; k < width - 1; k++) {
                 idx = 0;
                 newR = 0;
                 newG = 0;
@@ -277,16 +292,53 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //自定义函数 打开图片
-    public void OpenImage()
-    {
+    public void OpenImage() {
         Intent intent = new Intent(Intent.ACTION_PICK,
                 android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         startActivityForResult(intent, IMAGE_OPEN);
     }
+
+
+    public static boolean saveImageToGallery(Context context, Bitmap ab) {
+        // 首先保存图片
+        String storePath = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "dearxy";
+        File appDir = new File(storePath);
+        if (!appDir.exists()) {
+            appDir.mkdir();
+        }
+        String fileName = System.currentTimeMillis() + ".jpg";
+        File file = new File(appDir, fileName);
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            //通过io流的方式来压缩保存图片
+            boolean isSuccess = ab.compress(Bitmap.CompressFormat.JPEG, 60, fos);
+            fos.flush();
+            fos.close();
+
+            //把文件插入到系统图库
+            //MediaStore.Images.Media.insertImage(context.getContentResolver(), file.getAbsolutePath(), fileName, null);
+
+            //保存图片后发送广播通知更新数据库
+            Uri uri = Uri.fromFile(file);
+            context.sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, uri));
+            if (isSuccess) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
     //显示打开图片
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (Build.VERSION.SDK_INT >= 23 && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        }
         super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode==RESULT_OK && requestCode==IMAGE_OPEN) {
+        if (resultCode == RESULT_OK && requestCode == IMAGE_OPEN) {
             Uri imageFileUri = data.getData();
             DisplayMetrics dm = new DisplayMetrics();
             getWindowManager().getDefaultDisplay().getMetrics(dm);
@@ -297,14 +349,13 @@ public class MainActivity extends AppCompatActivity {
                 BitmapFactory.Options bmpFactoryOptions = new BitmapFactory.Options();
                 bmpFactoryOptions.inJustDecodeBounds = true;
                 bmp = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageFileUri), null, bmpFactoryOptions);
-                int heightRatio = (int)Math.ceil(bmpFactoryOptions.outHeight/(float)height);
-                int widthRatio = (int)Math.ceil(bmpFactoryOptions.outWidth/(float)width);
+                int heightRatio = (int) Math.ceil(bmpFactoryOptions.outHeight / (float) height);
+                int widthRatio = (int) Math.ceil(bmpFactoryOptions.outWidth / (float) width);
                 //inSampleSize表示图片占原图比例 1表示原图
-                if(heightRatio>1&&widthRatio>1) {
-                    if(heightRatio>widthRatio) {
+                if (heightRatio > 1 && widthRatio > 1) {
+                    if (heightRatio > widthRatio) {
                         bmpFactoryOptions.inSampleSize = heightRatio;
-                    }
-                    else {
+                    } else {
                         bmpFactoryOptions.inSampleSize = widthRatio;
                     }
                 }
@@ -312,15 +363,14 @@ public class MainActivity extends AppCompatActivity {
                 bmpFactoryOptions.inJustDecodeBounds = false;
                 bmp = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageFileUri), null, bmpFactoryOptions);
                 imageShow.setImageBitmap(bmp);
-            }
-            catch(FileNotFoundException e) {
+            } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
         }  //end if
     }
+
     //图片怀旧处理
-    private void OldRemeberImage()
-    {
+    private void OldRemeberImage() {
         /*
          * 怀旧处理算法即设置新的RGB
          * R=0.393r+0.769g+0.189b
@@ -339,10 +389,8 @@ public class MainActivity extends AppCompatActivity {
         int newB = 0;
         int[] pixels = new int[width * height];
         bmp.getPixels(pixels, 0, width, 0, 0, width, height);
-        for (int i = 0; i < height; i++)
-        {
-            for (int k = 0; k < width; k++)
-            {
+        for (int i = 0; i < height; i++) {
+            for (int k = 0; k < width; k++) {
                 pixColor = pixels[width * i + k];
                 pixR = Color.red(pixColor);
                 pixG = Color.green(pixColor);
@@ -356,12 +404,14 @@ public class MainActivity extends AppCompatActivity {
         }
         bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
         imageShow.setImageBitmap(bitmap);
+        saveImageToGallery(this, bitmap);
+
     }
+
     //图片浮雕处理
 //底片效果也非常简单:将当前像素点的RGB值分别与255之差后的值作为当前点的RGB
 //灰度图像:通常使用的方法是gray=0.3*pixR+0.59*pixG+0.11*pixB
-    private void ReliefImage()
-    {
+    private void ReliefImage() {
         /*
          * 算法原理：(前一个像素点RGB-当前像素点RGB+127)作为当前像素点RGB值
          * 在ABC中计算B点浮雕效果(RGB值在0~255)
@@ -381,10 +431,8 @@ public class MainActivity extends AppCompatActivity {
         int newB = 0;
         int[] pixels = new int[width * height];
         bmp.getPixels(pixels, 0, width, 0, 0, width, height);
-        for (int i = 1; i < height-1; i++)
-        {
-            for (int k = 1; k < width-1; k++)
-            {
+        for (int i = 1; i < height - 1; i++) {
+            for (int k = 1; k < width - 1; k++) {
                 //获取前一个像素颜色
                 pixColor = pixels[width * i + k];
                 pixR = Color.red(pixColor);
@@ -392,9 +440,9 @@ public class MainActivity extends AppCompatActivity {
                 pixB = Color.blue(pixColor);
                 //获取当前像素
                 pixColor = pixels[(width * i + k) + 1];
-                newR = Color.red(pixColor) - pixR +127;
-                newG = Color.green(pixColor) - pixG +127;
-                newB = Color.blue(pixColor) - pixB +127;
+                newR = Color.red(pixColor) - pixR + 127;
+                newG = Color.green(pixColor) - pixG + 127;
+                newB = Color.blue(pixColor) - pixB + 127;
                 newR = Math.min(255, Math.max(0, newR));
                 newG = Math.min(255, Math.max(0, newG));
                 newB = Math.min(255, Math.max(0, newB));
@@ -404,9 +452,9 @@ public class MainActivity extends AppCompatActivity {
         bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
         imageShow.setImageBitmap(bitmap);
     }
+
     //图像模糊处理
-    private void FuzzyImage()
-    {
+    private void FuzzyImage() {
         /*
          * 算法原理：
          * 简单算法将像素周围八个点包括自身共九个点RGB值分别相加后平均,当前像素点的RGB值
@@ -414,7 +462,7 @@ public class MainActivity extends AppCompatActivity {
          * 高斯矩阵 int[] gauss = new int[] { 1, 2, 1, 2, 4, 2, 1, 2, 1 };
          * 将九个点的RGB值分别与高斯矩阵中的对应项相乘的和,再除以一个相应的值作为当前像素点的RGB
          */
-        int[] gauss = new int[] { 1, 2, 1, 2, 4, 2, 1, 2, 1 };  // 高斯矩阵
+        int[] gauss = new int[]{1, 2, 1, 2, 4, 2, 1, 2, 1};  // 高斯矩阵
         int delta = 16; // 除以值 值越小图片会越亮,越大则越暗
         int width = bmp.getWidth();
         int height = bmp.getHeight();
@@ -428,10 +476,8 @@ public class MainActivity extends AppCompatActivity {
         int[] pixels = new int[width * height];
         bmp.getPixels(pixels, 0, width, 0, 0, width, height);
         //循环赋值
-        for (int i = 1; i < height-1; i++)
-        {
-            for (int k = 1; k < width-1; k++)
-            {
+        for (int i = 1; i < height - 1; i++) {
+            for (int k = 1; k < width - 1; k++) {
                 pos = 0;
                 newR = 0;
                 newG = 0;
@@ -463,9 +509,9 @@ public class MainActivity extends AppCompatActivity {
         bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
         imageShow.setImageBitmap(bitmap);
     }
+
     //图片光照效果
-    private void SunshineImage()
-    {
+    private void SunshineImage() {
         /*
          * 算法原理：(前一个像素点RGB-当前像素点RGB+127)作为当前像素点RGB值
          * 在ABC中计算B点浮雕效果(RGB值在0~255)
@@ -491,10 +537,8 @@ public class MainActivity extends AppCompatActivity {
         float strength = 150F;  //光照强度100-150
         int[] pixels = new int[width * height];
         bmp.getPixels(pixels, 0, width, 0, 0, width, height);
-        for (int i = 1; i < height-1; i++)
-        {
-            for (int k = 1; k < width-1; k++)
-            {
+        for (int i = 1; i < height - 1; i++) {
+            for (int k = 1; k < width - 1; k++) {
                 //获取前一个像素颜色
                 pixColor = pixels[width * i + k];
                 pixR = Color.red(pixColor);
@@ -504,11 +548,10 @@ public class MainActivity extends AppCompatActivity {
                 newG = pixG;
                 newB = pixB;
                 //计算当前点到光照中心的距离,平面坐标系中两点之间的距离
-                int distance = (int) (Math.pow((centerY-i), 2) + Math.pow((centerX-k), 2));
-                if(distance < radius*radius)
-                {
+                int distance = (int) (Math.pow((centerY - i), 2) + Math.pow((centerX - k), 2));
+                if (distance < radius * radius) {
                     //按照距离大小计算增强的光照值
-                    int result = (int)(strength*( 1.0-Math.sqrt(distance) / radius ));
+                    int result = (int) (strength * (1.0 - Math.sqrt(distance) / radius));
                     newR = pixR + result;
                     newG = newG + result;
                     newB = pixB + result;
@@ -520,6 +563,31 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+        imageShow.setImageBitmap(bitmap);
+    }
+    public void YouHua()
+    {
+        Bitmap bitmap = Bitmap.createBitmap(bmp.getWidth(),
+                bmp.getHeight(), Bitmap.Config.RGB_565);
+        int color;
+        int Radio = 0;
+        int width = bmp.getWidth();
+        int height = bmp.getHeight();
+        Random rnd = new Random();
+        int iModel = 10;
+        int i = width - iModel;
+        while (i > 1)
+        {
+            int j = height - iModel;
+            while (j > 1)
+            {
+                int iPos = rnd.nextInt(100000) % iModel;
+                color = bmp.getPixel(i + iPos, j + iPos);
+                bitmap.setPixel(i, j, color);
+                j = j - 1;
+            }
+            i = i - 1;
+        }
         imageShow.setImageBitmap(bitmap);
     }
 }
